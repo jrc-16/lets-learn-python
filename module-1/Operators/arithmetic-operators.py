c58ad242c3621dba030c7c8ca3a1dbe4ce3cb7f3
jason <jrcarney16@googlemail.com>
2020-05-11 22:17:21 +0100

    #0002: Added arithmetic-operators file which demonstrates simple arithmetic

--- module-1/Operators/arithmetic-operators.py
+++ module-1/Operators/arithmetic-operators.py
@@ -1,19 +1,28 @@
 # Addition
-
+addition_one = 4 + 4
+print(addition_one)
 
 # Subtraction
-
+subtraction_one = 4 - 2
+print(subtraction_one)
 
 # Multiplication
-
+multiplication_one = 4 * 8
+print(multiplication_one)
 
 # Division
-
+division_one = int(10/5)
+print(division_one)
 
 # Exponentiation
-
+exponent_one = 2**10
+print(exponent_one)
+# 2*2*2*2*2*2*2*2*2*2
 
 # Modulus
-
+modulus_one = 8%6
+print(modulus_one)
 
 # Floor division
+floor_one = 12//3
+print(floor_one)
