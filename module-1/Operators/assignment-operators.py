8e0903cd766152d006dd4e4f99a818cd5020e712
jason <jrcarney16@googlemail.com>
2020-05-12 21:42:23 +0100

    #0003: Added assignment operators file and stub comparison ops file

---
+++ module-1/Operators/assignment-operators.py
@@ -0,0 +1,34 @@
+# Addition
+x = 10
+x += 10
+print(x)
+
+# Subtraction
+a = 10
+a -= 2
+print(a)
+
+# Multiplication
+b = 2
+b *= 10
+print(b)
+
+# Division
+c = 15
+c /= 5
+print(c)
+
+# Exponentiation
+d = 3 
+d **= 3
+print(d)
+
+# Modulus
+e = 5
+e %= 3
+print(e)
+
+# Floor division
+f = 15
+f //= 9
+print(f)
---
+++ module-1/Operators/comparison-operators.py
@@ -0,0 +1,18 @@
+# Equal
+# num_one = 10
+# num_two = 5
+# num_one > num_two
+
+# Not equal
+
+
+# Greater than
+
+
+# Less than
+
+
+# Greater than or equal to 
+
+
+# Less than or equal to
