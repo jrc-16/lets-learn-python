9df7dcf35083a3f63faec0a2c77e19dd4af0a3c9
jason <jrcarney16@googlemail.com>
2020-05-13 22:36:52 +0100

    #0004: Checking in complete comparison operators file and stub logical operators file which needs more thought

--- module-1/Operators/comparison-operators.py
+++ module-1/Operators/comparison-operators.py
@@ -1,18 +1,29 @@
 # Equal
-# num_one = 10
-# num_two = 5
-# num_one > num_two
+num_one = 11
+num_two = 10
+print(num_one == num_two)
 
 # Not equal
-
+num_three = 14
+num_four = 14
+print(num_three != num_four)
 
 # Greater than
-
+num_five = 18
+num_six = 19
+print(num_five > num_six)
 
 # Less than
-
+num_seven = 30
+num_eight = 20
+print(num_seven < num_eight)
 
 # Greater than or equal to 
-
+num_nine = 29
+num_ten = 30
+print(num_nine >= num_ten)
 
 # Less than or equal to
+num_eleven = 61
+num_twelve = 60
+print(num_eleven <= num_twelve)
---
+++ module-1/Operators/logical-operators.py
@@ -0,0 +1,18 @@
+# And 
+# value_one = False
+# value_two = True
+# print(value_one and value_two)
+
+# value_two = 10
+# value_three = 15
+# value_four = 30 
+# value_five = 20
+# print(value_two < value_three and value_four > value_five)
+
+# Or
+
+
+# Not
+value_twenty = False
+value_twentyone = False
+print( not(value_twenty and value_twentyone) )
