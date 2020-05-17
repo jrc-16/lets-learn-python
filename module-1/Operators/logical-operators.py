b90984d342573c2ab2ff29ee8dd59a37b5e4cabe
jason <jrcarney16@googlemail.com>
2020-05-17 21:06:18 +0100

    #0005: Checked in code for covering lesson about logical operators

--- module-1/Operators/logical-operators.py
+++ module-1/Operators/logical-operators.py
@@ -1,18 +1,32 @@
 # And 
-# value_one = False
-# value_two = True
+value_one = False
+value_two = False
 # print(value_one and value_two)
 
-# value_two = 10
-# value_three = 15
-# value_four = 30 
-# value_five = 20
-# print(value_two < value_three and value_four > value_five)
+value_three = 10
+value_four = 15
+value_five = 30
+value_six = 20
+# print(value_three > value_four and value_five > value_six)
 
 # Or
+value_seven = True
+value_eight = False
+# print(value_seven or value_eight)
 
+value_nine = 100
+value_ten = 15
+value_eleven = 30
+value_twelve = 20
+# print(value_nine < value_ten or value_eleven < value_twelve)
 
 # Not
-value_twenty = False
-value_twentyone = False
-print( not(value_twenty and value_twentyone) )
+value_thirteen = False
+value_fourteen = True
+# print( not(value_thirteen and value_fourteen) )
+
+value_fifteen = 10
+value_sixteen = 12
+print( not(value_fifteen < value_sixteen) )
+
+
